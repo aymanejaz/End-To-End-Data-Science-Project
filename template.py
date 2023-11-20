11cf9d7b5dc999d67d72e6f18de95aadcfd48114
aymanijaz <aymanijaz21@gmail.com>
2023-11-20 17:01:44 +0500

    template.py initial commit

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-project_name="End-To-End-Data-Science-Project"
+project_name="EndToEnd_DataScience_Project"
 
 list_of_files=[
     f"src/{project_name}/__init__.py",
